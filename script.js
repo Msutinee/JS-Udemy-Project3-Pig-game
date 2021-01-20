c811612d512117e8bcaf6a69b81ad8a87749a035
msutinee <msutinee@gmail.com>
2021-01-20 00:22:15 -0500

    Added roll dice btn

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -16,6 +18,8 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentPlayer = 0;
+let playerScore = 0;
 let currentScore = 0; // Cannot be inside
 //if that it will reset everytime when clicked
 
@@ -33,10 +37,15 @@ btnRoll.addEventListener('click', function () {
     // Add dice to current score
     //currentScore = currentScore + randomDiceNumber;
     currentScore += randomDiceNumber;
-    current0El.textContent = currentScore; //❗CHANG LATER
+    document.getElementById(
+      `current--${currentPlayer}`
+    ).textContent = currentScore;
   } else {
     // Switch to next player
-    currentScore += randomDiceNumber;
-    current1El.textContent = currentScore; //❗CHANG LATER
+    document.getElementById(`current--${currentPlayer}`).textContent = 0;
+    currentScore = 0;
+    currentPlayer = currentPlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
