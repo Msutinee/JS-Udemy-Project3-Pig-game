3d08acba0fe3abc68d91a79fc45f837b9b3774f5
msutinee <msutinee@gmail.com>
2021-01-20 00:05:03 -0500

    hlaf way done

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 'use strict';
+
+// document.querySelector('.dice').style.display = 'none';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0; // Cannot be inside
+//if that it will reset everytime when clicked
+
+//Rolling dice functionality
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  let randomDiceNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(randomDiceNumber);
+  // 2. display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${randomDiceNumber}.png`;
+  // 3. check for rolled 1
+  if (randomDiceNumber !== 1) {
+    // Add dice to current score
+    //currentScore = currentScore + randomDiceNumber;
+    currentScore += randomDiceNumber;
+    current0El.textContent = currentScore; //❗CHANG LATER
+  } else {
+    // Switch to next player
+    currentScore += randomDiceNumber;
+    current1El.textContent = currentScore; //❗CHANG LATER
+  }
+});
